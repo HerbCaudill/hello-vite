ec2e62caa58177599c8ef1db7345a87aee842b0a
HerbCaudill <herb@devresults.com>
2021-03-01 15:02:52 +0100

    tailwind.config.ts:

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,15 +1,13 @@
 ﻿import colors from 'windicss/colors'
-import typography from 'windicss/plugin/typography'
 
 import { defineConfig } from 'vite-plugin-windicss'
 
 export default defineConfig({
   darkMode: 'class',
-  plugins: [typography],
   theme: {
     extend: {
       colors: {
-        primary: colors.violet['600'],
+        primary: colors.violet[500],
       },
     },
   },
